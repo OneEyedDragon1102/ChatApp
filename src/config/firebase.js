c525efaf622f050d33a01c86171d85feacd33e86
OneEyedDragon1102 <adityakhamare29@gmail.com>
2024-05-17 00:02:04 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,20 +3,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//    apiKey: "AIzaSyB9nCgRuDlOzob37IzvOpgpvuZYNrbmnOc",
-//   authDomain: "chatter-37dc6.firebaseapp.com",
-//   projectId: "chatter-37dc6",
-//   storageBucket: "chatter-37dc6.appspot.com",
-//   messagingSenderId: "931769986875",
-//   appId: "1:931769986875:web:799b3ba5be12d778c4511e",
-//   measurementId: "G-VBWB75YFP2"
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyAyxhM66Wtg1zTa3s2w3kqKRcpNFhLTXe4",
   authDomain: "chater-78d2e.firebaseapp.com",
